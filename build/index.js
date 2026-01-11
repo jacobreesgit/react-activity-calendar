781f2adf497f6a7d792210259a3e9d060d4f0e91
jacobreesgit <jacobrees@me.com>
2026-01-11 13:00:29 +0000

    Add build output for direct GitHub install

---
+++ build/index.js
@@ -0,0 +1,6 @@
+'use client';
+export { A as ActivityCalendar } from './chunks/index-BMLlN_Tb.js';
+import 'react';
+import 'date-fns';
+import 'react/jsx-runtime';
+//# sourceMappingURL=index.js.map
